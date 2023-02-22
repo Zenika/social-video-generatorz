585a4a42835669e87e07bd5aad9e07bf03fc97a4
emeric <emericboy73@gmail.com>
2023-02-22 15:29:49 +0100

    Clean honepage and add small styles + create style for generate page

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -1,3 +1,11 @@
+import styles from './styles.module.css';
+
 export default function Page() {
-  return <h1>This page is to GENERATE videos</h1>;
+  return (
+    <main>
+      <h1 className={styles.title}>This page is to GENERATE videos</h1>
+      <span className={styles.underline}></span>
+      <a className={styles.homeBtn} href="/">Home</a>
+    </main>
+  );
 }
