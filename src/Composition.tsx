adc1f85f422b7b6588a33a826ae96376b8e4e316
emeric <emericboy73@gmail.com>
2023-02-23 11:32:46 +0100

    Setup eslint and prettier. Tested them by reformating the files and correct eslint error

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,20 +1,20 @@
 import {AbsoluteFill, Img, staticFile} from 'remotion';
 
 export const MyComposition = () => {
-    return (
-        <AbsoluteFill
-            style={{
-                justifyContent: "center",
-                alignItems: "center",
-                backgroundColor: "black"
-            }}
-        >
-            <Img
-                width={200}
-                height={200}
-                src={staticFile("/mono_zenika.svg")}
-                alt="Monogramme Zenika"
-            />
-        </AbsoluteFill>
-    );
+	return (
+		<AbsoluteFill
+			style={{
+				justifyContent: 'center',
+				alignItems: 'center',
+				backgroundColor: 'black',
+			}}
+		>
+			<Img
+				width={200}
+				height={200}
+				src={staticFile('/mono_zenika.svg')}
+				alt="Monogramme Zenika"
+			/>
+		</AbsoluteFill>
+	);
 };
