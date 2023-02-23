20a9e0beba8c84b867f79b889f8d7dea8472f205
emeric <emericboy73@gmail.com>
2023-02-23 09:51:39 +0100

    Review changes : changed html link to next link

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -1,11 +1,12 @@
 import styles from './styles.module.css';
+import Link from "next/link";
 
 export default function Page() {
   return (
     <main>
       <h1 className={styles.title}>This page is to GENERATE videos</h1>
       <span className={styles.underline}></span>
-      <a className={styles.homeBtn} href="/">Home</a>
+      <Link className={styles.homeBtn} href="/">Home</Link>
     </main>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from './page.module.css'
 
 export default function Home() {
@@ -19,7 +20,7 @@ export default function Home() {
         </div>
       </div>
 
-      <a className={styles.generateBtn} href="/generate">Generate</a>
+      <Link className={styles.generateBtn} href="/generate">Generate</Link>
     </main>
   )
 }
