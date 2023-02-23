1b4351f7186a1ada3803940e7271160850f13613
emeric <emericboy73@gmail.com>
2023-02-23 09:43:46 +0100

    Review changes : add Zenika monogram to composition and add the logo in the images file too

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,3 +1,20 @@
+import {AbsoluteFill, Img, staticFile} from 'remotion';
+
 export const MyComposition = () => {
-    return null;
+    return (
+        <AbsoluteFill
+            style={{
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "black"
+            }}
+        >
+            <Img
+                width={200}
+                height={200}
+                src={staticFile("/mono_zenika.svg")}
+                alt="Monogramme Zenika"
+            />
+        </AbsoluteFill>
+    );
 };
