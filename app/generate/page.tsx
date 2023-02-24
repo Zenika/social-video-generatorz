05d2fc64ca1ab7cb33ff1daac3cfbfbc5078924a
emeric <emericboy73@gmail.com>
2023-02-24 12:12:55 +0100

    Review changes: remove useless span and and nested styles that wasn't

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -1,11 +1,10 @@
 import styles from './styles.module.css';
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Page() {
   return (
     <main>
       <h1 className={styles.title}>This page is to GENERATE videos</h1>
-      <span className={styles.underline}></span>
       <Link className={styles.homeBtn} href="/">Home</Link>
     </main>
   );
