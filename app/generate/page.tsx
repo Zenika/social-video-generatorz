6e1bc677cbc3923b66a8a1a639936bc81e84b301
Emeric Boyadjian <72607059+Boyadjie@users.noreply.github.com>
2023-02-24 14:06:12 +0100

    core(setup): initialise styles (#17)

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -1,3 +1,11 @@
+import styles from './styles.module.css';
+import Link from 'next/link';
+
 export default function Page() {
-  return <h1>This page is to GENERATE videos</h1>;
+  return (
+    <main>
+      <h1 className={styles.title}>This page is to GENERATE videos</h1>
+      <Link className={styles.homeBtn} href="/">Home</Link>
+    </main>
+  );
 }
