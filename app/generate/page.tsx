2551a30df5abdc86bce4ad04f8206615a87da101
emeric <emericboy73@gmail.com>
2023-02-24 14:59:32 +0100

    Rebase from main with style

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -2,10 +2,12 @@ import styles from './styles.module.css';
 import Link from 'next/link';
 
 export default function Page() {
-  return (
-    <main>
-      <h1 className={styles.title}>This page is to GENERATE videos</h1>
-      <Link className={styles.homeBtn} href="/">Home</Link>
-    </main>
-  );
+	return (
+		<main>
+			<h1 className={styles.title}>This page is to GENERATE videos</h1>
+			<Link className={styles.homeBtn} href="/">
+				Home
+			</Link>
+		</main>
+	);
 }
