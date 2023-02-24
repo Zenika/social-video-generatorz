45e3edce4afc37794d85d8083812691cd7b55569
Emeric Boyadjian <72607059+Boyadjie@users.noreply.github.com>
2023-02-24 15:07:27 +0100

    chore(setup): prettier + eslint (#18)

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,27 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import styles from './page.module.css'
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './page.module.css';
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
+	return (
+		<main className={styles.main}>
+			<div className={styles.center}>
+				<Image
+					priority
+					className={styles.logo}
+					src="/next.svg"
+					alt="Next.js Logo"
+					width={180}
+					height={37}
+				/>
+				<div className={styles.thirteen}>
+					<Image priority src="/thirteen.svg" alt="13" width={40} height={31} />
+				</div>
+			</div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <Link className={styles.generateBtn} href="/generate">Generate</Link>
-    </main>
-  )
+			<Link className={styles.generateBtn} href="/generate">
+				Generate
+			</Link>
+		</main>
+	);
 }
--- src/Root.tsx
+++ src/Root.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { MyComposition } from "./Composition";
-import {Composition} from "remotion";
+import React from 'react';
+import {MyComposition} from './Composition';
+import {Composition} from 'remotion';
 
 export const RemotionRoot: React.FC = () => {
-    return (
-        <>
-            <Composition
-                id="firstCompo"
-                component={MyComposition}
-                durationInFrames={60}
-                fps={30}
-                width={1280}
-                height={720}
-            />
-        </>
-    );
+	return (
+		<>
+			<Composition
+				id="firstCompo"
+				component={MyComposition}
+				durationInFrames={60}
+				fps={30}
+				width={1280}
+				height={720}
+			/>
+		</>
+	);
 };
