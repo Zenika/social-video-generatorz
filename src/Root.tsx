e4fc25e0bacfe203efdd3776550c4759ef65d735
emeric <emericboy73@gmail.com>
2023-02-24 15:33:18 +0100

    rebase from main

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,19 +3,18 @@ import {Monogram} from './LogoZenika/Monogram';
 import {Composition, Folder} from 'remotion';
 
 export const RemotionRoot: React.FC = () => {
-    return (
-        <>
-          <Folder name="Technozaure">
-
-            <Composition
-                id="Monogram"
-                component={Monogram}
-                durationInFrames={60}
-                fps={30}
-                width={1280}
-                height={720}
-            />
-          </Folder>
-        </>
-    );
+	return (
+		<>
+			<Folder name="Technozaure">
+				<Composition
+					id="Monogram"
+					component={Monogram}
+					durationInFrames={60}
+					fps={30}
+					width={1280}
+					height={720}
+				/>
+			</Folder>
+		</>
+	);
 };
