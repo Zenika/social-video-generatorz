c6a2348cfd6b7ca6756a9896080fd5a68a845247
emeric <emericboy73@gmail.com>
2023-02-27 16:44:48 +0100

    remove unused test id

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -8,7 +8,7 @@ import {Monogram} from '@/src/LogoZenika/Monogram';
 export default function Page() {
 	return (
 		<main className={styles.generate}>
-			<h1 data-testid="title-id">This page is to GENERATE videos</h1>
+			<h1>This page is to GENERATE videos</h1>
 			<Player
 				controls
 				// autoPlay
