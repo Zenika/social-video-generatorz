308a80009c7380c0b11aaa9f26577d6a77a6e461
emeric <emericboy73@gmail.com>
2023-02-28 14:27:25 +0100

    Remove playwright tests and change for jest + react-testing-library

---
+++ __tests__/testTitle.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Page from '../app/generate/page';
+
+test('Get title', async () => {
+	render(<Page />);
+
+	expect(
+		screen.getByText('This page is to GENERATE videos')
+	).toBeInTheDocument();
+});
--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -2,23 +2,21 @@
 
 import styles from './styles.module.css';
 import Link from 'next/link';
-import {Player} from '@remotion/player';
-import {Monogram} from '@/src/LogoZenika/Monogram';
 
 export default function Page() {
 	return (
 		<main className={styles.generate}>
 			<h1>This page is to GENERATE videos</h1>
-			<Player
-				controls
-				// autoPlay
-				loop
-				durationInFrames={60}
-				fps={30}
-				compositionWidth={900}
-				compositionHeight={500}
-				component={Monogram}
-			/>
+			{/*<Player*/}
+			{/*	controls*/}
+			{/*	// autoPlay*/}
+			{/*	loop*/}
+			{/*	durationInFrames={60}*/}
+			{/*	fps={30}*/}
+			{/*	compositionWidth={900}*/}
+			{/*	compositionHeight={500}*/}
+			{/*	component={Monogram}*/}
+			{/*/>*/}
 			<Link className="btn" href="/">
 				Home
 			</Link>
