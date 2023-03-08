7cec52057089021d54731ad0b6e3933a9385fcc7
emeric <emericboy73@gmail.com>
2023-03-08 10:38:49 +0100

    Completed talk template

--- src/Components/Logo.tsx
+++ src/Components/Logo.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import {AbsoluteFill, Img, staticFile} from 'remotion';
+import {
+	AbsoluteFill,
+	Img,
+	spring,
+	staticFile,
+	useCurrentFrame,
+	useVideoConfig,
+} from 'remotion';
 
 export const Logo: React.FC<{src: string}> = ({src}) => {
+	const frame = useCurrentFrame();
+	const {fps} = useVideoConfig();
+
+	const slideTop = spring({
+		frame,
+		from: -200,
+		to: 80,
+		fps,
+		durationInFrames: 20,
+	});
+
 	return (
 		<AbsoluteFill
 			style={{
 				alignItems: 'center',
-				top: 80,
+				top: slideTop,
 			}}
 		>
 			<Img src={staticFile(src)} width={650} />
