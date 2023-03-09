734c025276b8d2ac833c4ef5ef091b74c4e37d33
emeric <emericboy73@gmail.com>
2023-03-09 14:03:58 +0100

    Completed talk template

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Monogram} from './LogoZenika/Monogram';
 import {Composition, Folder} from 'remotion';
+import {BotzTalk} from './BestOfTechnozaure/Talk/BotzTalk';
 import {Intro} from './BestOfTechnozaure/Event/Intro';
 import {BotzEvent} from './BestOfTechnozaure/Event/BotzEvent';
 import {Tribes} from './BestOfTechnozaure/Event/Tribes';
@@ -124,6 +125,30 @@ export const RemotionRoot: React.FC = () => {
 						}}
 					/>
 				</Folder>
+				<Composition
+					id="BotzTalk"
+					component={BotzTalk}
+					width={1200}
+					height={1200}
+					fps={30}
+					durationInFrames={150}
+					defaultProps={{
+						title:
+							'Live-coding : d’une archi 3 couches à une archi hexagonale, grâce au TDD',
+						date: '09 novembre 2023',
+						speaker: {
+							name: 'Mickaël Alves',
+							picture:
+								'https://pbs.twimg.com/profile_images/1452247219709566977/5Xzmgun-_400x400.jpg',
+							role: 'Consultant frontend',
+							location: 'lyon',
+						},
+						category: {
+							name: 'CRAFTMANSHIP',
+							icon: '/BestOfTz/icons/Dev.svg',
+						},
+					}}
+				/>
 			</Folder>
 		</>
 	);
