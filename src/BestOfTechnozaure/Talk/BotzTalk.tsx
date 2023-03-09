4c07382b4c858d88e71d30666594f2dd56cb225e
emeric <emericboy73@gmail.com>
2023-03-09 14:03:58 +0100

    Completed talk template

---
+++ src/BestOfTechnozaure/Talk/BotzTalk.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import {loadFont} from '@remotion/google-fonts/Nunito';
+
+import {
+	AbsoluteFill,
+	Sequence,
+	spring,
+	useCurrentFrame,
+	useVideoConfig,
+} from 'remotion';
+import {Background} from '../../Components/Background';
+import {CategoryProps, SpeakerProps} from '../../Types';
+import {Speaker} from '../../Components/Speaker/Speaker';
+import {Title} from '../../Components/Title';
+import {Logo} from '../../Components/Logo';
+import {LogoDate} from './LogoDate';
+import {Website} from '../../Components/Website';
+
+const {fontFamily} = loadFont();
+
+export interface BotzTalkProps {
+	title: string;
+	date: string;
+	speaker: SpeakerProps;
+	category: CategoryProps;
+}
+
+export const BotzTalk: React.FC<BotzTalkProps> = ({
+	title,
+	date,
+	speaker,
+	category,
+}) => {
+	const frame = useCurrentFrame();
+	const {fps} = useVideoConfig();
+
+	const durationInFrames = 30;
+	const delay = 10;
+	const slideLeft = spring({
+		frame: frame - delay,
+		fps,
+		from: 770,
+		to: 670,
+		durationInFrames,
+	});
+	const opacity = spring({
+		frame: frame - 10,
+		from: 0,
+		to: 1,
+		fps,
+		durationInFrames,
+	});
+
+	return (
+		<AbsoluteFill style={{color: 'white', fontFamily}}>
+			<Background url="/BestOfTz/background_carre.png" />
+			<Website url="www.zenika.com" />
+			<Sequence name="Logo">
+				<Logo src="/BestOfTz/BOTZ_LOGO.png" width={650} top={80} />
+			</Sequence>
+			<Sequence name="Speaker" from={15}>
+				<Speaker
+					picture={speaker.picture}
+					name={speaker.name}
+					role={speaker.role}
+					location={speaker.location}
+				/>
+			</Sequence>
+			<Sequence name="Title" from={40}>
+				<Title title={title} style={{fontSize: '3.6rem'}} top={800} />
+			</Sequence>
+			<Sequence name="Detail" from={60}>
+				<LogoDate date={date} />
+			</Sequence>
+		</AbsoluteFill>
+	);
+};
