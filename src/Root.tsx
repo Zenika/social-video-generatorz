bc9f939e8048c65d5b425ff8e4c96265a28759a5
emeric <emericboy73@gmail.com>
2023-03-09 10:21:32 +0100

    WIP: remove talk

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import {Monogram} from './LogoZenika/Monogram';
 import {Composition, Folder} from 'remotion';
-import {BotzTalk} from './BestOfTechnozaure/Talk/BotzTalk';
 import {Intro} from './BestOfTechnozaure/Event/Intro';
 import {BotzEvent} from './BestOfTechnozaure/Event/BotzEvent';
+
+const defaultPropsWithSpeaker = {
+	title:
+		'Live-coding : d’une archi 3 couches à une archi hexagonale, grâce au TDD',
+	date: '09 novembre 2023',
+	speaker: {
+		name: 'Mickaël Alves',
+		picture:
+			'https://pbs.twimg.com/profile_images/1452247219709566977/5Xzmgun-_400x400.jpg',
+	},
+	category: {
+		name: 'CRAFTMANSHIP',
+		icon: '/BestOfTz/icons/Dev.svg',
+	},
+};
 
 export const RemotionRoot: React.FC = () => {
 	return (
@@ -16,28 +30,6 @@ export const RemotionRoot: React.FC = () => {
 					fps={30}
 					width={1280}
 					height={720}
-				/>
-				<Composition
-					id="BotzTalk"
-					component={BotzTalk}
-					width={1200}
-					height={1200}
-					fps={30}
-					durationInFrames={150}
-					defaultProps={{
-						title:
-							'Live-coding : d’une archi 3 couches à une archi hexagonale, grâce au TDD',
-						date: '09 novembre 2023',
-						speaker: {
-							name: 'Mickaël Alves',
-							picture:
-								'https://pbs.twimg.com/profile_images/1452247219709566977/5Xzmgun-_400x400.jpg',
-						},
-						category: {
-							name: 'CRAFTMANSHIP',
-							icon: '/BestOfTz/icons/Dev.svg',
-						},
-					}}
 				/>
 				<Folder name="Event">
 					<Composition
