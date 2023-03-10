61e5071d0c4053a7a27a85ed26f3f7b9e24de08f
emeric <emericboy73@gmail.com>
2023-03-10 16:20:39 +0100

    Remove Event video

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import {Monogram} from './LogoZenika/Monogram';
 import {Composition, Folder} from 'remotion';
-import {Intro} from './BestOfTechnozaure/Event/Intro';
-import {BotzEvent} from './BestOfTechnozaure/Event/BotzEvent';
-import {Tribes} from './BestOfTechnozaure/Event/Tribes';
-import {Contact} from './BestOfTechnozaure/Event/Contact';
 
 export const RemotionRoot: React.FC = () => {
 	return (
@@ -18,112 +14,6 @@ export const RemotionRoot: React.FC = () => {
 					width={1280}
 					height={720}
 				/>
-				<Folder name="Event">
-					<Composition
-						id="Intro"
-						component={Intro}
-						width={1200}
-						height={1200}
-						fps={30}
-						durationInFrames={140}
-						defaultProps={{
-							title: 'Venez assister à des conférences techs',
-							city: 'lyon',
-							date: '28 Novembre 2023',
-							time: '17h00',
-							onlinePlatform: 'workadventure',
-						}}
-					/>
-					<Composition
-						id="Categories"
-						component={Tribes}
-						width={1200}
-						height={1200}
-						fps={30}
-						durationInFrames={110}
-						defaultProps={{
-							categories: [
-								{
-									name: 'Green IT',
-									icon: '/BestOfTz/icons/GreenIT.svg',
-								},
-								{
-									name: 'Sécurité',
-									icon: '/BestOfTz/icons/Sécurité.svg',
-								},
-								{
-									name: 'Cloud',
-									icon: '/BestOfTz/icons/Cloud.svg',
-								},
-								{
-									name: 'Craftsmanship',
-									icon: '/BestOfTz/icons/Dev.svg',
-								},
-							],
-						}}
-					/>
-					<Composition
-						id="Contact"
-						component={Contact}
-						width={1200}
-						height={1200}
-						fps={30}
-						durationInFrames={70}
-						defaultProps={{
-							contact: {
-								name: 'Lucas Audart',
-								picture:
-									'https://ca.slack-edge.com/T02ARLB3P-U02H7C4H6PJ-eb62023feb1e-512',
-								role: 'Consultant frontend',
-								location: 'lyon',
-								mail: 'lucas.audart@zenika.com',
-								phone: '06 57 29 32 64',
-							},
-						}}
-					/>
-					<Composition
-						id="BotzEvent"
-						component={BotzEvent}
-						width={1200}
-						height={1200}
-						fps={30}
-						durationInFrames={300}
-						defaultProps={{
-							title: 'L’évenement imaginé par les Techs pour les Techs',
-							city: 'lyon',
-							date: '28 Novembre 2023',
-							time: '17h00',
-							onlinePlatform: 'workadventure',
-							contact: {
-								name: 'Lucas Audart',
-								picture:
-									'https://ca.slack-edge.com/T02ARLB3P-U02H7C4H6PJ-eb62023feb1e-512',
-								role: 'Consultant frontend',
-								location: 'lyon',
-								mail: 'lucas.audart@zenika.com',
-								phone: '06 57 29 32 64',
-							},
-							categories: [
-								{
-									name: 'Green IT',
-									icon: '/BestOfTz/icons/GreenIT.svg',
-								},
-								{
-									name: 'Sécurité',
-									icon: '/BestOfTz/icons/Sécurité.svg',
-								},
-								{
-									name: 'Cloud',
-									icon: '/BestOfTz/icons/Cloud.svg',
-								},
-								{
-									name: 'Craftsmanship',
-									icon: '/BestOfTz/icons/Dev.svg',
-								},
-							],
-						}}
-					/>
-				</Folder>
 			</Folder>
 		</>
 	);
