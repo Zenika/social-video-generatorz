09619a9361617571b0d8203e0315904ad6586f47
emeric <emericboy73@gmail.com>
2023-03-15 09:16:20 +0100

    Refacto duplicated default props

--- app/[videoName]/page.tsx
+++ app/[videoName]/page.tsx
@@ -7,53 +7,8 @@ import {BotzEvent} from '../../src/BestOfTechnozaure/Event/BotzEvent';
 import {BotzTalk} from '../../src/BestOfTechnozaure/Talk/BotzTalk';
 import Link from 'next/link';
 import Image from 'next/image';
-
-const sampleEventData = {
-	title: 'L’évenement imaginé par les Techs pour les Techs',
-	city: 'lyon',
-	date: '28 Novembre 2023',
-	time: '17h00',
-	onlinePlatform: 'workadventure',
-	contact: {
-		name: 'Lucas Audart',
-		picture: 'https://ca.slack-edge.com/T02ARLB3P-U02H7C4H6PJ-eb62023feb1e-512',
-		role: 'Consultant frontend',
-		location: 'lyon',
-		mail: 'lucas.audart@zenika.com',
-		phone: '06 57 29 32 64',
-	},
-	categories: [
-		{
-			name: 'Green IT',
-			icon: '/BestOfTz/icons/GreenIT.svg',
-		},
-		{
-			name: 'Sécurité',
-			icon: '/BestOfTz/icons/Sécurité.svg',
-		},
-		{
-			name: 'Cloud',
-			icon: '/BestOfTz/icons/Cloud.svg',
-		},
-		{
-			name: 'Craftsmanship',
-			icon: '/BestOfTz/icons/Dev.svg',
-		},
-	],
-};
-
-const sampleTalkData = {
-	title:
-		'Live-coding : d’une archi 3 couches à une archi hexagonale, grâce au TDD',
-	date: '09 novembre 2023',
-	speaker: {
-		name: 'Mickaël Alves',
-		picture:
-			'https://pbs.twimg.com/profile_images/1452247219709566977/5Xzmgun-_400x400.jpg',
-		role: 'Consultant frontend',
-		location: 'lyon',
-	},
-};
+import {EventDefaultProps} from '../../src/DefaultProps/EventDefaultProps';
+import {TalkDefaultProps} from '../../src/DefaultProps/TalkDefaultProps';
 
 interface TalkTemplate {
 	component: React.FC<any>;
@@ -71,7 +26,7 @@ const Template: Record<string, TalkTemplate> = {
 		width: 1200,
 		height: 1200,
 		durationInFrames: 300,
-		sampleData: sampleEventData,
+		sampleData: EventDefaultProps,
 	},
 	BotzTalk: {
 		compositionName: 'BotzTalk',
@@ -79,7 +34,7 @@ const Template: Record<string, TalkTemplate> = {
 		width: 1200,
 		height: 1200,
 		durationInFrames: 150,
-		sampleData: sampleTalkData,
+		sampleData: TalkDefaultProps,
 	},
 };
 export default function Page({params}: {params: {videoName: string}}) {
@@ -106,9 +61,6 @@ export default function Page({params}: {params: {videoName: string}}) {
 					style={{width: 500, height: 500}}
 					inputProps={data}
 				/>
-				<div className={styles.form}>
-					<form></form>
-				</div>
 			</div>
 		</main>
 	);
