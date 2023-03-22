2bd1a8d2501fe5e5295c8e458501948e02276cb5
Emeric Boyadjian <72607059+Boyadjie@users.noreply.github.com>
2023-03-22 16:58:45 +0100

    feat(website): resolve build errors + add categories (#41)

--- src/DefaultProps/CategoriesList.ts
+++ src/DefaultProps/CategoriesList.ts
@@ -5,18 +5,58 @@ type CategoriesListProps = Record<string, CategoryProps>;
 export const CategoriesList: CategoriesListProps = {
 	greenit: {
 		name: 'Green IT',
-		icon: '/BestOfTz/icons/GreenIT.svg',
+		icon: '/BestOfTz/icons/greenit.svg',
 	},
 	securite: {
 		name: 'Sécurité',
-		icon: '/BestOfTz/icons/Sécurité.svg',
+		icon: '/BestOfTz/icons/securite.svg',
 	},
 	cloud: {
 		name: 'Cloud',
-		icon: '/BestOfTz/icons/Cloud.svg',
+		icon: '/BestOfTz/icons/cloud.svg',
 	},
 	craft: {
 		name: 'Craftsmanship',
-		icon: '/BestOfTz/icons/Dev.svg',
+		icon: '/BestOfTz/icons/dev.svg',
+	},
+	dev: {
+		name: 'Backend/Frontend',
+		icon: '/BestOfTz/icons/dev.svg',
+	},
+	devops: {
+		name: 'Devops',
+		icon: '/BestOfTz/icons/devops.svg',
+	},
+	innovation: {
+		name: 'Innovation',
+		icon: '/BestOfTz/icons/innovation.svg',
+	},
+	agilite: {
+		name: 'Agilité',
+		icon: '/BestOfTz/icons/agilite.svg',
+	},
+	architecture: {
+		name: 'Architecture',
+		icon: '/BestOfTz/icons/architecture.svg',
+	},
+	mobile: {
+		name: 'Mobile',
+		icon: '/BestOfTz/icons/mobile.svg',
+	},
+	support: {
+		name: 'Fonctions Support',
+		icon: '/BestOfTz/icons/support.svg',
+	},
+	data: {
+		name: 'Data',
+		icon: '/BestOfTz/icons/data.svg',
+	},
+	design: {
+		name: 'Design',
+		icon: '/BestOfTz/icons/design.svg',
+	},
+	ia: {
+		name: 'IA',
+		icon: '/BestOfTz/icons/ia.svg',
 	},
 };
