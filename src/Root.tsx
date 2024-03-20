f3f6efb52f790bcace51abb39055afcd23d23019
jeanphi.baconnais <jeanphilippe.baconnais@gmail.com>
2024-03-20 15:09:44 +0000

    :sparkles: Add Gitpod configuration file & init Zenikast component

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Monogram} from './LogoZenika/Monogram';
+import {Teaser} from './ZenikastComm/Teaser';
+import { Monogram } from './LogoZenika/Monogram';
 import {Composition, Folder} from 'remotion';
 import {BotzTalk} from './BestOfTechnozaure/Talk/BotzTalk';
 import {Intro} from './BestOfTechnozaure/Event/Intro';
@@ -144,6 +145,17 @@ export const RemotionRoot: React.FC = () => {
 							location: 'lyon',
 						},
 					}}
+				/>
+			</Folder>
+
+			<Folder name="Zenikast">
+				<Composition
+					id="ZenikastCom"
+					component={Teaser}
+					durationInFrames={60}
+					fps={30}
+					width={1200}
+					height={1200}
 				/>
 			</Folder>
 		</>
